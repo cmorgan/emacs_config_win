e82b5a98fc4b6591dcb6320a7d06cef0ec82f9df
Chris Morgan <christopher.morgan@solo.com>
2014-10-09 08:30:35 +0100

    update

--- user.el
+++ user.el
@@ -84,6 +84,22 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 
+(setq
+ python-shell-interpreter "C:\\dev\\bin\\Anaconda\\python.exe"
+ python-shell-interpreter-args "-i C:\\dev\\bin\\Anaconda\\Scripts\\ipython-script.py"
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+;; Themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(add-to-list 'load-path "~/.emacs.d/themes")
+(load-theme 'tomorrow-night-bright t)
 
 (provide 'user)
 ;;; user.el ends here
