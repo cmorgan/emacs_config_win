4d90f4f3ad8269c001e416cfd39dedf626f80768
Chris Morgan <cmorgan@gazprom-mt.com>
2016-02-24 18:14:55 +0000

    check

--- user.el
+++ user.el
@@ -342,7 +342,7 @@ See URL `http://flowtype.org/'."
 	  "f" 'projectile-find-file
     "k" 'projectile-kill-buffers
     "s" 'projectile-switch-project
-    "b" 'projectile-switch-to-buffer
+    ;;"sb" 'projectile-switch-to-buffer
     )
 
   (global-evil-leader-mode) 
@@ -417,6 +417,8 @@ See URL `http://flowtype.org/'."
         (setq exec-path (append exec-path '("C:\\dev\\bin\\git\\bin"))) 
         ))
 
+(require 'flx-ido)
+;; disable ido faces to see flx highlights.
 
 (use-package ido
   :ensure t
@@ -424,7 +426,9 @@ See URL `http://flowtype.org/'."
 	 "IDO"
 	 (ido-mode t)
 	 (ido-everywhere t)
+   (flx-ido-mode 1)
 	 (setq ido-enable-flex-matching t)
+   (setq ido-use-faces nil)
 	 (setq ido-file-extensions-order '(".py" ".js" ".emacs" t))
      (setq 
       ido-save-directory-list-file "~/.emacs.d/cache/ido.last"
@@ -476,8 +480,8 @@ See URL `http://flowtype.org/'."
 
 (setq
  ;;python-shell-interpreter "C:\\dev\\bin\\Anaconda\\envs\\bmra\\python.exe"
- ;;python-shell-interpreter "C:\\dev\\bin\\Anaconda\\envs\\dev\\python.exe"
- python-shell-interpreter "C:\\dev\\bin\\Anaconda\\envs\\pys\\python.exe"
+ python-shell-interpreter "C:\\dev\\bin\\Anaconda\\envs\\dev\\python.exe"
+ ;;python-shell-interpreter "C:\\dev\\bin\\Anaconda\\envs\\pys\\python.exe"
  python-shell-interpreter-args "-i C:\\dev\\bin\\Anaconda\\Scripts\\ipython-script.py"
  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
@@ -487,6 +491,14 @@ See URL `http://flowtype.org/'."
    "';'.join(module_completion('''%s'''))\n"
  python-shell-completion-string-code
    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+(defun toggle-pyenv ()
+  (interactive)
+  (message "test!!")
+(princ "Hello world!  I'm writing to STDOUT but I'm not in quotes!")
+ python-shell-interpreter "C:\\dev\\bin\\Anaconda\\envs\\dev\\python.exe"
+  )
+(global-set-key (kbd "C-c C-d") 'toggle-pyenv)
 
 ;; Themes
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
@@ -545,6 +557,14 @@ See URL `http://flowtype.org/'."
 (global-set-key (kbd "C-c s") 'toggle-window-split-horiz-vert)
 
 
+(defun test-fmt ()
+  (message "Hello, %s!" "message to stderr")
+  (fmt-stdout "Hello, %s!\n" "fmt-stdout, explict newline")
+  (fmtln-stdout "Hello, %s!" "fmtln-stdout, implicit newline"))
+
+(global-set-key (kbd "C-c s") 'toggle-window-split-horiz-vert)
+
+
 (eval-after-load 'magit
   '(progn
      (set-face-foreground 'magit-diff-add "green3")
@@ -580,80 +600,6 @@ See URL `http://flowtype.org/'."
     :include-back t
     :end-not-begin t)))
 (mmm-add-mode-ext-class 'python-mode nil 'python-rst)
-
-;; This makes ido-find-file-other-window,
-;; ido-switch-buffer-other-window, et. al obsolete. It’s a much better
-;; abstraction, and I believe it should become apart of ido mode,
-;; because any command that uses ido-completing-read can benefit from
-;; it without any additional effort, including textmate.el’s
-;; textmate-goto-symbol.
-
-
-(defun split-window-vertically-and-switch ()
-  (interactive)
-  (split-window-vertically)
-  (other-window 1))
-
-(defun split-window-horizontally-and-switch ()
-  (interactive)
-  (split-window-horizontally)
-  (other-window 1))
-
-(defun ido-invoke-in-other-window ()
-  "signals ido mode to switch to (or create) another window after exiting"
-  (interactive)
-  (setq ido-exit-minibuffer-target-window 'other)
-  (ido-exit-minibuffer))
-
-(defun ido-invoke-in-horizontal-split ()
-  "signals ido mode to split horizontally and switch after exiting"
-  (interactive)
-  (setq ido-exit-minibuffer-target-window 'horizontal)
-  (ido-exit-minibuffer))
-
-(defun ido-invoke-in-vertical-split ()
-  "signals ido mode to split vertically and switch after exiting"
-  (interactive)
-  (setq ido-exit-minibuffer-target-window 'vertical)
-  (ido-exit-minibuffer))
-
-(defun ido-invoke-in-new-frame ()
-  "signals ido mode to create a new frame after exiting"
-  (interactive)
-  (setq ido-exit-minibuffer-target-window 'frame)
-  (ido-exit-minibuffer))
-
-(defadvice ido-read-internal (around ido-read-internal-with-minibuffer-other-window activate)
-  (let* (ido-exit-minibuffer-target-window
-         (this-buffer (current-buffer))
-         (result ad-do-it))
-    (cond
-     ((equal ido-exit-minibuffer-target-window 'other)
-      (if (= 1 (count-windows))
-          (split-window-horizontally-and-switch)
-        (other-window 1)))
-     ((equal ido-exit-minibuffer-target-window 'horizontal)
-      (split-window-horizontally-and-switch))
-
-     ((equal ido-exit-minibuffer-target-window 'vertical)
-      (split-window-vertically-and-switch))
-     ((equal ido-exit-minibuffer-target-window 'frame)
-      (make-frame)))
-    (switch-to-buffer this-buffer) ;; why? Some ido commands, such as textmate.el's textmate-goto-symbol don't switch the current buffer
-    result))
-
-(defadvice ido-init-completion-maps (after ido-init-completion-maps-with-other-window-keys activate)
-  (mapcar (lambda (map)
-            (define-key map (kbd "C-o") 'ido-invoke-in-other-window)
-            (define-key map (kbd "C-2") 'ido-invoke-in-vertical-split)
-            (define-key map (kbd "C-3") 'ido-invoke-in-horizontal-split)
-            (define-key map (kbd "C-4") 'ido-invoke-in-other-window)
-            (define-key map (kbd "C-5") 'ido-invoke-in-new-frame))
-          (list ido-buffer-completion-map
-                ido-common-completion-map
-                ido-file-completion-map
-                ido-file-dir-completion-map)))
-
 
 (provide 'user)
 ;;; user.el ends here
