6e078a709af79759fdbaaa8233f66d0085774718
Chris Morgan <cmorgan@gazprom-mt.com>
2016-09-26 11:10:12 +0100

    tidy

--- init.el
+++ init.el
@@ -1,15 +1,8 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-(require 'init-benchmarking) ;; Measure startup time
-;;----------------------------------------------------------------------------
-;; Temporarily reduce garbage collection during startup
-;;----------------------------------------------------------------------------
-(defconst sanityinc/initial-gc-cons-threshold gc-cons-threshold
-  "Initial value of `gc-cons-threshold' at start-up time.")
-(setq gc-cons-threshold (* 128 1024 1024))
-(add-hook 'after-init-hook
-          (lambda () (setq gc-cons-threshold sanityinc/initial-gc-cons-threshold)))
 
 (require 'package)
+
+(require 'user)
 
 ;;; Code:
 (setq package-archives '(
@@ -24,19 +17,18 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(load "~/.emacs.d/user.el")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("c:/projects/projects.org" "c:/projects/priviento.org" "c:/projects/sean.org"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; (load "~/.emacs.d/user.el")
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(org-agenda-files (quote ("c:/projects/projects.org" "c:/projects/priviento.org" "c:/projects/sean.org"))))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
 
-
-(put 'dired-find-alternate-file 'disabled nil)
+;; (put 'dired-find-alternate-file 'disabled nil)
---
+++ lisp/coding.el
@@ -0,0 +1,49 @@
+
+(use-package flycheck
+  :ensure t
+  :init (timeit
+	 "FLYCHECK"
+	 (add-hook 'after-init-hook #'global-flycheck-mode)
+	 (setq flycheck-highlighting-mode 'lines)
+	 (setq flycheck-ghc-language-extensions ())
+	 (setq python-check-function "flake8")
+	 (flycheck-define-checker javascript-flow
+	   "A JavaScript syntax and style checker using Flow. 
+See URL `http://flowtype.org/'."
+	   :command ("flow" source-original)
+	   :error-patterns
+	   ((error line-start
+	   	   (file-name)
+	   	   ":"
+	   	   line
+	   	   ":"
+	   	   (minimal-match (one-or-more (not (any ":"))))
+	   	   ": "
+	   	   (message (minimal-match (and (one-or-more anything) "\n")))
+	   	   line-end))
+	   :modes js-mode)
+	 (add-to-list 'flycheck-checkers 'javascript-flow t)
+	 (flycheck-add-next-checker 'javascript-gjslint 'javascript-flow)))
+
+
+;; (add-hook 'image-mode-hook 'eimp-mode)
+(setq image-dired-external-viewer "C:\\Program Files (x86)\\IrfanView\\i_view32.exe")
+
+
+(use-package web-mode
+  :init
+  (progn
+    (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.tmpl\\'" . web-mode))
+    (add-hook 'webmode-mode-hook '(auto-fill-mode -1))
+
+    ))
+
+(provide 'code)
+
